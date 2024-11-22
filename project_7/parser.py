f262be0f37683a1936c53315ecb3b903824e4b49
spencer-harbin <sdh7301@gmail.com>
2024-11-21 21:13:17 -0500

    start proj7 parser

---
+++ project_7/parser.py
@@ -0,0 +1,18 @@
+class Parser:
+    def __init__(self): 
+        pass 
+
+    def has_more_commands(self):
+        pass
+
+    def advance(self): 
+        pass
+
+    def command_type(self): 
+        pass
+
+    def arg_1(self): 
+        pass 
+
+    def arg_2(self): 
+        pass 
